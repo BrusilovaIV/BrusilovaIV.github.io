e18e47b071aa1ca7d92f865b0b8b64eaaad0b33d
m-d-z-s <ira.brusilova@list.ru>
2022-12-11 14:25:51 +0300

    add countDown

---
+++ js/main.js
@@ -0,0 +1,29 @@
+function updateCountdown(){
+    const year = document.querySelector('#year');
+    const days = document.querySelector('#days');
+    const hours = document.querySelector('#hours');
+    const minutes = document.querySelector('#minutes');
+    const seconds = document.querySelector('#seconds');
+    
+    const currentTime = new Date();
+    const currentYear = new Date().getFullYear();
+    const nextYear = new Date(`January 01 ${currentYear +1} 00:00:00`);
+    const diff = nextYear - currentTime;
+    const daysLeft = Math.floor (diff/1000/60/60/24);
+    const hoursLeft = Math.floor(diff/1000/60/60)%24;
+    const minutesLeft = Math.floor(diff/1000/60)%60;
+    const secondsLeft = Math.floor(diff/1000)%60;
+    
+    year.innerText = currentYear + 1;
+    hours.innerText =  hoursLeft < 10? `0` + hoursLeft :hoursLeft;
+    days.innerText = daysLeft < 10? `0` + daysLeft : daysLeft;
+    minutes.innerText = minutesLeft < 10? `0` + minutesLeft :minutesLeft;
+    seconds.innerText = secondsLeft < 10? `0` + secondsLeft :secondsLeft;
+    
+    console.log(daysLeft);
+    console.log(hoursLeft);
+    console.log(minutesLeft);
+    console.log(secondsLeft);
+    }
+updateCountdown();
+setInterval(updateCountdown, 1000);
